ecceae6ef5789a357b8c481e5b538933f11bef0c
TahaBouregba <mohamedtahabouregba@gmail.com>
2026-09-25 11:22:00 +0100

    Add: is-palindrome - submission-2

---
+++ Data Structures & Algorithms/is-palindrome/submission-2.py
@@ -0,0 +1,14 @@
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        t=""
+        
+        for i in s:
+            if i.isalnum():
+                t += i.lower()
+        n=len(t)
+        for i in range (n):
+            if t[i] != t[n-1-i]:
+                return False
+        return True
+
